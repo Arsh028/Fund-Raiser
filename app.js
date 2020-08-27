b7366ea11efb5f5b26063ad5ca74279e0e2790fe
Arsh Radhanpura <arshradhanpura28@gmail.com>
2020-08-27 14:31:30 +0530

    changed basic HTML to ejs templating

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const ejs = require("ejs");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));//put all css in public file in order for it to work
@@ -27,17 +30,15 @@ const UserSchema = new mongoose.Schema(
 var User = new mongoose.model("User", UserSchema);
 
 // render home page
-app.get("/",(req,res)=>res.sendFile(__dirname + "/Home.html"));
-
+app.get("/",(req,res)=>res.render("pages/Home"));
 
 
 //============================REGISTER PAGE==============================
 
 // render register page
-app.get("/Register",(req,res)=>res.sendFile(__dirname + "/Register.html"));
+app.get("/Register",(req,res)=>res.render("pages/Register"));
 
 app.post("/Register",function(req,res){
-    console.log("inside register");
     const v1 = req.body.pass;
     const v2 = req.body.newpass;
     if(v1 == v2)
@@ -60,6 +61,7 @@ app.post("/Register",function(req,res){
                 else
                 {
                     //TODO -- create home page of user depending upon type and put the patch below
+                    console.log(req.body.email + " got registered");
                     res.redirect("/");
                 }
             });
@@ -76,25 +78,28 @@ app.post("/Register",function(req,res){
 
 //============================LOGIN PAGE=============================
 
-app.get("/Login",(req,res)=>res.sendFile(__dirname + "/Login.html"));
+//render login page
+app.get("/Login",(req,res)=>res.render("pages/Login"));
 
 app.post("/Login",function(req,res){
     console.log("inside login");
     const input_email = req.body.email;
     User.findOne({email : input_email},function(err,foundUser){
-        if(err){console.log(err);}
+        if(err){console.log("user not found");res.redirect("/Register");}
         else
         {
             bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
                 if(result == true)
                 {
-                    console.log("found user");    
+                    console.log(foundUser.email+" user logged in");  
                     //TODO -- create home page of user depending upon type and put the patch below
                     res.redirect("/");
                 }
                 else
                 {
-                    console.log("not found");
+                
+                    //TODO -- show error of this
+                    console.log("password incorrect");
                 }
             });
         }
